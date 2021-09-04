5b97cd2b019f4144a370b7c61d4ea364e788e9d2
mafuzal <mafuzalhoque@gmail.com>
2021-09-04 12:23:45 -0400

    Section 3:- Statements and Operators -Logical Operators need to be revisited.

---
+++ 3_StatementsOperatorsBasics/3_LogicalOperatorsPractice.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Define the conversion values in cents 
+	const int dollar_value = 100;
+	const int quarter_value = 25;
+	const int dime_value = 10;
+	const int nickel_value = 5;
+	
+	int change_amount;
+	
+	//Solution 1 will not use modulo operator 
+	cout<<"Enter the amount in cents : ";
+	cin>>change_amount;
+	
+	int balance, dollars, quarters, dimes, nickels, pennies;
+	dollars = change_amount/dollar_value;
+	balance = change_amount - (dollars * dollar_value);
+	
+	quarters = balance/quarter_value;
+	balance -= quarters*quarter_value;
+	
+	dimes = balance/dime_value;
+	balance -= dimes*dime_value;
+	
+	nickels = balance/nickel_value;
+	balance -= nickels*nickel_value; 
+	
+	pennies = balance; 
+	
+	cout<<"\n You can provide this change as follows: "<<endl;
+	cout<<"Dollars: "<<dollars<<endl;
+	cout<<"Quarters: "<<quarters<<endl;
+	cout<<"Dimes: "<<dimes<<endl;
+	cout<<"Nickels: "<<nickels<<endl;
+	cout<<"Pennies: "<<pennies<<endl;
+	
+	
+	
+	//Solution 2 using the modulo operator:
+	balance = dollars = quarters = dimes = nickels = pennies = 0; 
+	dollars = change_amount / dollar_value; 
+	balance = change_amount % dollar_value;
+	
+	quarters = balance / quarter_value;
+	balance %= quarter_value;
+	
+	dimes = balance / dime_value;
+	balance %= dime_value;
+	
+	nickels = balance / nickel_value;
+	balance %= nickel_value;
+	
+	pennies = balance;
+	
+	cout<<"\n You can provide this change as follows: "<<endl;
+	cout<<"Dollars: "<<dollars<<endl;
+	cout<<"Quarters: "<<quarters<<endl;
+	cout<<"Dimes: "<<dimes<<endl;
+	cout<<"Nickels: "<<nickels<<endl;
+	cout<<"Pennies: "<<pennies<<endl;	
+	
+	
+	cout<<endl;
+	cout<<endl;
+
+	return 0;
+	
+}
