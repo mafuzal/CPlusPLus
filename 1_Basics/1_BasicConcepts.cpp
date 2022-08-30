87f5afdf0d9f69b4ddbf4f4366906b760bc9473f
mafuzalhoque <mafuzalhoque@gmail.com>
2022-08-30 05:00:51 -0400

    C++ Basics S1.1 Changes

--- 1_Basics/1_BasicConcepts.cpp
+++ 1_Basics/1_BasicConcepts.cpp
@@ -1,31 +1,16 @@
-/*
- * Topics Covered:
- * 1.Pre-Processor
- * 2.Main Types
+/**
+ * Basics - S1.1
+ * C++ 14 
+ * Basic C++ Demo. 
  */
-#include <iostream>
-/*
-Note: #include is a type of pre-processor directive
-Preprocesors are programs that processes the source code before
-compiler sees them. Other examples of pre-processor are
-#ifdef, #indef, #define, #undef, #if, #elif, #else, #endif
- */
-int main()
-/*
- * This is the main function.
- * It is a special function, and every program must have excatly 1 main function.
- * It is the starting point of a program excution.
- * It will return 0 for succesful program execution.
- * Consider the other main declration
- * int main( int argc, char *argv[])
- * argc is argument count and argv is argument vector
- */
-{
-	int x;
-	std::cout << "Enter a number to get its multiple of 5:";
-	std::cin >> x;
-	std::cout <<"Number you entered is: "<<x<<std::endl;
-	std::cout << "5 X "<<x<<" = "<< 5*x<<std::endl;
-	return 0;
-}
 
+#include <iostream>
+int main()
+{
+	int xVal;
+	std::cout<<"Enter a value between 1 and 50:";
+	std::cin>>xVal;
+	std::cout<<"Number entered is: "<<xVal<<std::endl;
+	return 0;
+
+}
